19d0361a6beb098f80e8796f02f430a54855c8ba
abaart <abaart@gmail.com>
2015-12-01 15:16:14 +0100

    further progress in implementing test use case

--- FlaskKasadaka/kasadaka.py
+++ FlaskKasadaka/kasadaka.py
@@ -12,20 +12,20 @@ def index():
     return 'This is the Kasadaka Vxml generator'
 
 @app.route('/message.vxml')
-def message(messageURL = "interface/error.wav"):
-    return render_template("message.vxml",messageaudio = config.audioURL + messageURL)
+def message(state = "",messageURL = "interface/error.wav"):
+    return render_template("message.vxml",messageaudio = config.audioURL + messageURL, state = state)
 
 @app.route('/menu.vxml')
-def menu(questionURL,options):
-    return render_template('menu.vxml', questionaudio = config.audioURL + questionURL, questionaudiodir = config.interfaceURL, results = options)
+def menu(state,questionURL,options):
+    return render_template('menu.vxml', state = state, questionaudio = config.audioURL + questionURL, questionaudiodir = config.interfaceURL, results = options)
 
 @app.route('/confirm.vxml')
 def confirm(subjectURL):
     return ""
 
 @app.route('/result.vxml')
-def result(results):
-    return ""
+def result(state,messageAudio,results):
+    return render_template('result.vxml',state = state, messageAudio = messageAudio, results = results)
 
 @app.route('/post.vxml')
 def post():
@@ -34,7 +34,14 @@ def post():
 
 @app.route('/main.vxml')
 def main():
-    return callhandler.start()
+    if 'state' in request.args and 'data' in request.args:
+        return callhandler.handle(request.args['state'],request.args['data'])
+    elif 'state' in request.args:
+        return callhandler.handle(request.args['state'])
+    elif 'data' in request.args:
+        return callhandler.handle(request.args['data'])
+    else:
+        return callhandler.handle()
     #return menu("interface/samplequestion.wav",executeSparqlQuery("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     #PREFIX speakle: <http://purl.org/collections/w4ra/speakle/>
     #PREFIX radiomarche: <http://purl.org/collections/w4ra/radiomarche/>
