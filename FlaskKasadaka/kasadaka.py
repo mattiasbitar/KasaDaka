0463227e38ab2abc8f6ef714b57674dfb01ea8dc
abaart <abaart@gmail.com>
2015-12-01 16:47:44 +0100

    Testing new implementation
    Only getting info about product implemented.
    Also removed unused code in Sparql lib

--- FlaskKasadaka/kasadaka.py
+++ FlaskKasadaka/kasadaka.py
@@ -11,6 +11,21 @@ app = Flask(__name__)
 def index():
     return 'This is the Kasadaka Vxml generator'
 
+@app.route('/main.vxml')
+def main():
+    options = [
+            ['requestProductOfferings.vxml',config.interfaceURL+'requestProductOfferings.wav'],
+            ['chooseProductToMakeOffer',config.interfaceURL+'chooseProductToMakeOffer.wav']
+            ]
+
+
+    return render_template(
+    'main.vxml',
+    interfaceAudioDir = config.interfaceURL,
+     welcomeAudio = 'welcome.wav',
+     questionAudio = "mainMenuQuestion.wav",
+     options = options)
+
 @app.route('/message.vxml')
 def message(state = "",messageURL = "interface/error.wav"):
     return render_template("message.vxml",messageaudio = config.audioURL + messageURL, state = state)
@@ -27,31 +42,60 @@ def confirm(subjectURL):
 def result(state,messageAudio,results):
     return render_template('result.vxml',state = state, messageAudio = messageAudio, results = results)
 
-@app.route('/post.vxml')
-def post():
-    return ""
+
+@app.route('/requestProductOfferings.vxml')
+def requestProductOfferings():
+
+    #als het product meegegeven wordt, resultaten laten zien (loop back naar main), anders lijst met te kiezen producten
+    if 'choice' in request.args:
+        choice = request.args['choice']
+        #TODO keuze in query maken
+        results = executeSparqlQuery(
+            """PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX speakle: <http://purl.org/collections/w4ra/speakle/>
+            PREFIX radiomarche: <http://purl.org/collections/w4ra/radiomarche/>
+            SELECT DISTINCT ?person ?voicelabel_en  WHERE {
+                     ?person  rdf:type radiomarche:Person  .
+                     ?person radiomarche:contact_fname ?fname .
+                     ?person radiomarche:contact_lname ?lname.
+                     ?person speakle:voicelabel_en ?voicelabel_en
+            }
+            LIMIT """
+
+            )
+        return render_template(
+            'result.vxml',
+            interfaceAudioDir = config.interfaceURL,
+            messageAudio = 'presentProductOfferings.wav',
+            redirect = 'main.vxml',
+            results = results)
 
 
-@app.route('/main.vxml')
-def main():
-    options = [
-            ['requestProductOfferings.vxml','interface/requestProductOfferings.wav'],
-            ['chooseProductToMakeOffer','interface/chooseProductToMakeOffer.wav']
-            ]
+    #if no choice was made, offer choices of products to get offerings from
+    choices = executeSparqlQuery(
+        """PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX speakle: <http://purl.org/collections/w4ra/speakle/>
+        PREFIX radiomarche: <http://purl.org/collections/w4ra/radiomarche/>
+        SELECT DISTINCT ?person ?voicelabel_en  WHERE {
+                 ?person  rdf:type radiomarche:Person  .
+                 ?person radiomarche:contact_fname ?fname .
+                 ?person radiomarche:contact_lname ?lname.
+                 ?person speakle:voicelabel_en ?voicelabel_en
+        }
+        LIMIT """
+
+        )
+    for choice in choices:
+        choice[0] = 'requestProductOfferings.vxml?choice=' + choice[0]
+    return render_template(
+    'menu.vxml',
+    options = choices,
+    interfaceAudioDir = config.interfaceURL,
+    questionAudio = "chooseYourProduct.wav"
+    )
 
 
-    return render_template('main.vxml', interfaceAudioDir = 'interface/', welcomeAudio = 'welcome.wav', questionAudio = "mainMenuQuestion.wav", options = options)
 
-    #return menu("interface/samplequestion.wav",executeSparqlQuery("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    #PREFIX speakle: <http://purl.org/collections/w4ra/speakle/>
-    #PREFIX radiomarche: <http://purl.org/collections/w4ra/radiomarche/>
-    #SELECT DISTINCT ?person ?voicelabel_en  WHERE {
-    #          ?person          rdf:type radiomarche:Person  .
-    #                     ?person radiomarche:contact_fname ?fname .
-    #                                ?person radiomarche:contact_lname ?lname.
-    #                                                  ?person speakle:voicelabel_en ?voicelabel_en
-    #}
-    #LIMIT 5"""))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",debug=config.debug)
