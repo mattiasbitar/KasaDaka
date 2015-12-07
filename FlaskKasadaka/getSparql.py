9d10e333ec989ffbcb25590a79a0fe8069e100fb
abaart <abaart@gmail.com>
2015-12-07 14:42:32 +0100

    1

--- FlaskKasadaka/getSparql.py
+++ FlaskKasadaka/getSparql.py
@@ -32,7 +32,7 @@ def executeSparqlQuery(query, url = config.sparqlURL):
         results.append([])
         for item in result:
             for content in item:
-                toAppend = content.text
+                toAppend = urllib.quote_plus(content.text)
                 toAppend = toAppend.replace("-","%2D")
                 results[len(results)-1].append(toAppend)
 
