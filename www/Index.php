b7378acdd125c0fcf3bba8be1061a207b8094737
mattiasbitar <mattiasbitar@hotmail.se>
2016-05-25 13:11:35 +0200

    testWebpage

---
+++ www/Index.php
@@ -0,0 +1,54 @@
+<!----------------------------------------
+  --  Kristian ELawad & Mattias Bitar   --
+  --    Indek åk4 - Databas projekt     --
+  ---------------------------------------->
+  
+<!-- 
+Å = &Aring;
+å = &aring; 
+Ä = &Auml;
+ä = &auml; 
+Ö = &Ouml;
+ö = &ouml; 
+-->
+
+
+<html>
+<head>
+<title> Dalarnas Stugby </title>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+
+<!-- Table for Main Body -->
+<table border="0" width="100%" cellspacing="0">
+
+<tr><td align="left" valign="bottom" height="35" width="100%" nowrap colspan="4" style="padding: 20px;">
+<h1>Dalarnas Stugby</h1>
+</td></tr>
+
+<tr><td valign="top" align="left" style="min-width:140px; padding-left:20px; padding-right:20px; font-size: 18px;">
+
+
+<a href="index.php" style="text-decoration: none;">Hem </a> <br>
+<hr>
+<a href="page2.php" style="text-decoration: none;">Hyr stuga</a> <br>
+<hr>
+<a href="page3.php" style="text-decoration: none;">Boka utrustning</a> <br>
+
+</td>
+<td valign="top" bgcolor="white" width="100% "style="padding: 20px;">
+
+<h2> V&auml;lkommen till Dalarnas stugby</h2>
+<hr>  
+
+Hej och v&auml;lkommen till stugbyn i dalarna, anv&auml;nd l&auml;nkarna i sidan till att hyra stuga eller att boka utrustning.
+<br>
+<br>
+
+</td></tr>
+
+</table>
+
+</body>
+</html>
